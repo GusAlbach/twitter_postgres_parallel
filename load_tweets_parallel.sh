d6b971ee12d5227dca03525fbb8a2d9ac894912b
GusAlbach <zepolalbach@gmail.com>
2024-04-12 21:48:24 -0700

    small timer change

--- load_tweets_parallel.sh
+++ load_tweets_parallel.sh
@@ -5,11 +5,11 @@ files=$(find data/*)
 echo '================================================================================'
 echo 'load pg_denormalized'
 echo '================================================================================'
-time echo "$files" | parallel ./load_denormalized.sh
+echo "$files" | time parallel ./load_denormalized.sh
 echo '================================================================================'
 echo 'load pg_normalized'
 echo '================================================================================'
-time echo "$files" | parallel ./load_normalized.sh
+echo "$files" | time parallel ./load_normalized.sh
 echo '================================================================================'
 echo 'load pg_normalized_batch'
 echo '================================================================================'
